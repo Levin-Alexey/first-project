a1d87ba8edd67d97b8025c4c9a14e36b80bdb3fe
Levin_Alexey <levin.penza@yandex.ru>
2022-01-29 17:59:41 +0300

    Мой первый коммит!

---
+++ Praktikum.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Scanner;// Импортируйте нужные классы
+
+public class Praktikum {
+    public static void main(String[] args) {
+        System.out.println("Hello Leha!!!")
+    }
+}
