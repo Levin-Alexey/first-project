f914da9e17a183e4deaea52a34ba7fd294832782
Levin_Alexey <levin.penza@yandex.ru>
2022-01-29 18:04:55 +0300

    Объявил методы

---
+++ Converter.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Scanner;// Импортируйте нужные классы
+
+public class Converter {
+    public static void main(String[] args) {
+        System.out.println("Hello Leha Levin!!!")
+    }
+}
